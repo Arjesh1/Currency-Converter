21bdfbf5b3a8b596571284f626fefa7d87ac8948
Arjesh Khadka <109441944+Arjesh1@users.noreply.github.com>
2023-04-03 21:53:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 
 
+function amtTyping(){
+    const amt = document.getElementById('inputAmt').value
+    console.log(amt);
+
+
+    amt= document.getElementById('fromCurrency').innerHTML 
+
+    console.log(amt);
+    
+
+
+}
 
 
 
-fetch ("https://api.frankfurter.app/currencies")
-then((data) => JSON())
-then((data) =>{
-    display(data)
-})
+
+
+// fetch ("https://api.frankfurter.app/currencies")
+// .then((data) => data.json())
+// .then((data) =>{
+//     display(data)
+// })
+
+// function display(data){
+//     const entries = Object.entries(data)
+//     for(var i=0; i < entries.length; i++){
+//         select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+//         select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+//     }
+// }
 
 
