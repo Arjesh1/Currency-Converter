9784ec1d079509a0156fe5239cc60b2cfcf37d0e
Arjesh Khadka <109441944+Arjesh1@users.noreply.github.com>
2023-04-08 22:05:25 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 
 
 const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
-const num = document.getElementById("inputAmt");
 const ans = document.getElementById("ans");
 
+
+function handleOnChange(){
+  const input = document.getElementById("inputAmt")
+  console.log(input);
+}
+
+let formData = document.getElementById("form");
+
+formData.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    num = document.getElementById("inputAmt").value
+    console.log(num);
+
+   
+    
+}
+    )
 
 
 fetch("https://api.frankfurter.app/currencies")
