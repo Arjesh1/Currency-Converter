0b91d9fe06668b755c616a4f8b2e85d2ce346e44
Arjesh Khadka <109441944+Arjesh1@users.noreply.github.com>
2023-04-10 22:03:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-
 const api = "https://api.exchangerate-api.com/v4/latest/USD";
 
-const select = document.querySelectorAll(".currency");
-const ans = document.getElementById("ans");
+let ans = document.getElementById("ans");
+var fromCurrency = document.getElementById("fromCurrency")
+var toCurrency = document.getElementById("toCurrency")
+let inputForm = document.getElementById("inputForm")
+let convert = document.getElementById("submit")
+var finalValue = document.getElementById("toValue");
+var resultFrom;
+var resultTo;
+var inputAmt;
 
 
-function handleOnChange(){
-  const input = document.getElementById("inputAmt")
-  console.log(input);
+fromCurrency.addEventListener('change', (event) => {
+    resultFrom = `${event.target.value}`;
+    console.log(resultFrom);
+
+});
+  
+
+toCurrency.addEventListener('change', (event) => {
+    resultTo = `${event.target.value}`;
+    console.log(resultTo);
+});
+
+inputForm.addEventListener('input', updateValue);
+  
+
+function updateValue(e) {
+    inputAmt = e.target.value;
+    console.log(inputAmt);
 }
 
-let formData = document.getElementById("form");
 
-formData.addEventListener("click", (e) => {
-    e.preventDefault();
 
-    num = document.getElementById("inputAmt").value
-    console.log(num);
+convert.addEventListener("click", getResults);
+  
 
-   
+function getResults() {
+    fetch(`${api}`)
+        .then(currency => {
+            return currency.json();
+        }).then(displayResults);
+}
+  
+
+function displayResults(currency) {
+    if (resultFrom === resultTo){
+        alert ("Please select different currencies")
+    } else{
+    let fromRate = currency.rates[resultFrom];
+    let toRate = currency.rates[resultTo];
+
+    let ans = 
+       ((toRate / fromRate) * inputAmt).toFixed(2);
     
+
+    console.log(ans);
 }
-    )
-
-
-
-
+}
